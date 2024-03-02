4e82fc7fff5a934506fed92c038f964d1a2c0993
luke-bradbury-95 <luke-bradbury-95@hotmail.com>
2024-03-02 13:29:13 +0000

    Added a methld

--- Program.cs
+++ Program.cs
@@ -1,27 +1,28 @@
 ﻿using System.Globalization;
 
 Animals tiger = new Animals ("Tiger", "Asia", "Large");
-Console.WriteLine (tiger.ToString ());
-Console.WriteLine (tiger.name);
-Console.WriteLine(tiger.habitat);
-Console.WriteLine (tiger.size);
-Console.ReadLine ();
-
+Console.WriteLine (tiger.Name);
+Console.WriteLine(tiger.Habitat);
+Console.WriteLine (tiger.Size);
+Console.WriteLine(tiger.ShowAnimal());
+Console.ReadLine();
 class Animals
 {
-    public string name;     //Setting the properties of the class
-    public string habitat;
-    public string size;
+    public string Name;     //Setting the properties of the class
+    public string Habitat;
+    public string Size;
     public DateTime DateTime;
 
     public Animals(string name, string habitat, string size) // Needed to create constructors
     {
-        this.name = name;
-        this.habitat = habitat;
-        this.size = size;
-    }   
+        this.Name = name;
+        this.Habitat = habitat;
+        this.Size = size;
+    }
+    public string ShowAnimal()
+=> $"Animal name is {Name}, Habitat is {Habitat}, Size is {Size}";
     /* int CalculateCircumference(){    How a method would work inside a class
      * return Width*Height;
      * } */
-    
+
 }
