554b663c520f596ab818cd15e29a6c47355cda58
luke-bradbury-95 <luke-bradbury-95@hotmail.com>
2024-03-02 12:20:11 +0000

    Basics of creating classes and a dummy method inside of it

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+Animals tiger = new Animals ("Tiger", "Asia", "Large");
+Console.WriteLine (tiger.ToString ());
+Console.WriteLine (tiger.name);
+Console.WriteLine(tiger.habitat);
+Console.WriteLine (tiger.size);
+Console.ReadLine ();
+
+class Animals
+{
+    public string name;     //Setting the properties of the class
+    public string habitat;
+    public string size;
+    public DateTime DateTime;
+
+    public Animals(string name, string habitat, string size) // Needed to create constructors
+    {
+        this.name = name;
+        this.habitat = habitat;
+        this.size = size;
+    }   
+    /* int CalculateCircumference(){    How a method would work inside a class
+     * return Width*Height;
+     * } */
+    
+}
